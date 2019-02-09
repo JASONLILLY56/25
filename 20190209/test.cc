a52ca77486f35736fcf22f5c1024bddf349aea1a
Jason <jason@JasondeMacBook-Pro.local>
2019-02-09 11:39:01 +0800

    review

---
+++ 20190209/test.cc
@@ -0,0 +1,15 @@
+///
+///@file     test.cc
+///@author     Jason(13933536151@163.com)
+///@data     2019-02-09 11:35:1549683344
+///
+  
+#include<iostream>
+using std::cout;
+using std::cin;
+using std::endl;//作用域限定符
+using namespace std;//using编译指令 
+int main()
+{
+    using std::cout;//using 声明机制
+}
